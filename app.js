436bbc275d74614dff9eeaad23fe9b91856bbf48
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-20 14:39:19 +0000

    adding

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var app = express.createServer();
+var app = express();
  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
