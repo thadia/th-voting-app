c2e4df7803cef084c7506ebfc422331619bb58c6
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-20 14:34:43 +0000

    adding

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
  
 var routes = require("./routes/routes.js")(app);
  
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT, function () {
     console.log("Listening on port %s...", server.address().port);
 });
