3494f27993214b3a6e0fd4f0db14b9c5ca3599c1
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-23 13:48:18 +0000

    fixing

--- search.js
+++ search.js
@@ -1,9 +1,9 @@
 module.exports = { 
     findTiny: function(tiny, map_global){
-      for(var i=0;i<100;i++){
-        if(map_global[i][1] == tiny )
-          return map_global[i][0];
-      }
+     // for(var i=0;i<100;i++){
+     //   if(map_global[i][1] == tiny )
+          return map_global[tiny][0];
+     // }
     }
 };
 
