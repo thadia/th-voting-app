2a138035dee7fa5686851f67806efbbec76790e2
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-24 15:54:35 +0000

    fixing

--- search.js
+++ search.js
@@ -2,8 +2,14 @@ module.exports = {
     findTiny: function(tiny, map_global){
      // for(var i=0;i<100;i++){
      //   if(map_global[i][1] == tiny )
+     if(map_global[tiny][0] === undefined ) {
+       return "Not Found!";
+     }
+     else {
+          console.log(map_global[tiny][0]);
           return map_global[tiny][0];
      // }
+      }
     }
 };
 
