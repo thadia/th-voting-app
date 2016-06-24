ec92c4d416779f74842874a64a93fc77f5eed879
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-24 16:05:14 +0000

    fixing

--- search.js
+++ search.js
@@ -3,7 +3,7 @@ module.exports = {
      // for(var i=0;i<100;i++){
      //   if(map_global[i][1] == tiny )
      if(map_global[tiny][0] === undefined ) {
-       return "Not Found!";
+       return false;
      }
      else {
           console.log(map_global[tiny][0]);
