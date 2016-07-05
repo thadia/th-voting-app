ca670c68899f786e88b5b74c8bf7d8ad732bc1b8
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-05 17:19:26 +0000

    code in

---
+++ public/core.js
@@ -0,0 +1,27 @@
+var listApp = angular.module('listApp',[]);
+
+
+function listController($scope, $http) {
+    $scope.formData = {};
+$http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+    
+     // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+}
