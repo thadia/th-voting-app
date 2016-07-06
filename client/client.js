7afa3431612c4e2b1c78ef42bdc23206d0c55b26
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-06 20:43:29 +0000

    code in

--- client/client.js
+++ client/client.js
@@ -1,18 +1,22 @@
- 
-function AppViewModel() {
-   
-   //  this.firstName= $.getJSON("/polls/all").stringify();
-     
-      var self = this;
-      this.firstName;
 
-     
-    $.getJSON( "/polls/all", function( data ) {
-        self.firstName=data;
-        console.log(data);
-      });
-    
-}
+var AppViewModel = function() { //firstName
+    var self = this;
+    this.todoItems = ko.observableArray();
+ 
+    this.refresh = ko.command(function() {
+        //make a call to the server...
+        return $.getJSON("/polls/all");
+    }).done(function(items) {
+        //...and update the todoItems collection when the call returns
+        var newItems = [];
+        for (var i=0; i < items.length; i++ ){
+            newItems.push(new AppViewModel(items[i]));
+        }
+        self.todoItems(newItems);
+    });
+    //refresh immediately to load initial data
+    this.refresh();
+};
 
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
