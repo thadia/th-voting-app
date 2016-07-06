e30e726a960a5a32e53b3fa8f31bbebb3f56afac
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-06 17:06:53 +0000

    code in

--- public/client.js
+++ public/client.js
@@ -1,6 +1,8 @@
 var ko = require('knockout');// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 
 
+
+
 function AppViewModel() {
     this.firstName = "Bert";
     this.lastName = "Bertington";
