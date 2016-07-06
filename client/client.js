ece040d74df6b1c2f7fb5a7a03c859c94e70ace8
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-06 19:17:21 +0000

    code in

--- client/client.js
+++ client/client.js
@@ -1,31 +1,13 @@
-//var ko = require('knockout');// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-//
+ 
 function AppViewModel() {
-    this.firstName = "Bert";
-    this.lastName = "Bertington";
+    this.firstName = poll_list;
+    
 }
 
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
 
-/*
-
-module.exports = { 
-    AppViewModel: function(){
-     try{
-          this.firstName = "Bert";
-          this.lastName = "Bertington";
-      }
-     catch(err){
-        return false;
-       
-     }  
-    }
-};
-
-
-
-*/
+ 
 
 
 
