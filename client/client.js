41ab98b14097ae89dfb2f16d16723b2375731847
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-07 14:43:04 +0000

    code in

--- client/client.js
+++ client/client.js
@@ -2,16 +2,12 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('voteController', ['$scope', function($scope) {
  
- 
-      $http.get('/polls/all').
-          success(function (data, status, headers, config) {
+      $.getJSON('/polls/all', function(data){
+         
             $scope.data=data;
-            console.log("LOG "+ data + ":::" + data[0] + ":::" + data[0].title)
-          }).
-          error(function (data, status, headers, config) {
-            // ...
-          });
- 
+            console.log("LOG "+ data + ":::" + data[0] + ":::" + data[0].title);
+      });        
+           
 }]);
  
 
