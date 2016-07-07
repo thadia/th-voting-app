8e537b3ccdcb2e85b3e524d98cee41e75508a201
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-07 13:54:28 +0000

    code in

--- client/client.js
+++ client/client.js
@@ -2,15 +2,18 @@
 var AppViewModel = function() { //firstName
     var self = this;
     this.todoItems = ko.observableArray();
- 
+  
     this.refresh = ko.command(function() {
         //make a call to the server...
         return $.getJSON("/polls/all");
     }).done(function(items) {
         //...and update the todoItems collection when the call returns
-       
-        self.todoItems(items[0].title);
-    });
+       for (var i=0; i < items.length; i++ ){
+            var newItems = [];
+            newItems.push(new TodoViewModel(items[i].title , items[i].list));
+        }
+         self.todoItems(newItems);
+     });
     //refresh immediately to load initial data
     this.refresh();
 };
