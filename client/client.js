99295d00fdd32c0cdf3bcf78ead201709a84387f
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-07 14:36:15 +0000

    code in

--- client/client.js
+++ client/client.js
@@ -1,30 +1,17 @@
-function PollSchemaObject(data) {
-    this.title = ko.observable(data.title);
-    this.list = ko.observable(data.list);
-}
+var myApp = angular.module('myApp',[]);
 
-var PollViewModel = function() { //firstName
-    var self = this;
-    
-    this.todoItems = ko.observableArray();
-  
-    this.refresh = ko.command(function() {
-        //make a call to the server...
-        return $.getJSON("/polls/all");
-    }).done(function(items) {
-        //...and update the todoItems collection when the call returns
-       console.log("Items:  "+items);
-       self.todoItems(items);
-     });
-    //refresh immediately to load initial data
-    this.refresh();
-};
-
-// Activates knockout.js
-ko.applyBindings(new PollViewModel());
-
+myApp.controller('voteController', ['$scope', function($scope) {
  
-
-
-
+ 
+      $http.get('/polls/all').
+          success(function (data, status, headers, config) {
+            $scope.data=data;
+            console.log("LOG "+ data + ":::" + data[0] + ":::" + data[0].title)
+          }).
+          error(function (data, status, headers, config) {
+            // ...
+          });
+ 
+}]);
+ 
 
