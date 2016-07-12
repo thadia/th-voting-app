c100c1bfc50bdfbebc706e72c90afb7e8647e578
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-12 19:49:14 +0000

    adding owner and voters

--- client/client.js
+++ client/client.js
@@ -6,16 +6,7 @@ myApp.controller('mainController', function($scope, $http) {
         $scope.polls = response.data;});
         
 
-    $scope.showSelectValue = function(mySelect) {
-    console.log(mySelect);
-}
-   
-   
-   $scope.secondMethod = function() {
-      $http.get("/polls/vote/:user/:poolName/:mySelect")
-      .then(function (response) {
-          $scope.polls = response.data;});
-};
+    
        
 }); 
 
