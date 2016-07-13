83d4853a4e2902835a509a9762c327bd7acc3b34
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-13 17:41:52 +0000

    adding owner and voters

--- client/client.js
+++ client/client.js
@@ -3,10 +3,25 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('mainController', function($scope, $http) {
      $http.get("/polls/all")
     .then(function (response) {
-        $scope.polls = response.data;});
+        $scope.polls = response.data;
+        
+    });
         
 
-    
+     $scope.vote = function() {
+         //voting call here
+         var string_API = "/polls/vote/:user/:poolName/:" + $scope.selectedName;
+         console.log(string_API);
+         
+         
+    /*     
+         $http.get("/polls/vote/:user/:poolName/:itemType")  //string 
+        .then(function (response) {
+             $scope.polls = response.data;}); */
+          
+         
+         
+    };
        
 }); 
 
