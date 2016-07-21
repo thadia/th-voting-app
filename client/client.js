30d9f988d8299b3da0f560c6f915d635b903bcff
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-21 15:40:05 +0000

    code in

--- client/client.js
+++ client/client.js
@@ -5,45 +5,8 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('mainController', function($scope, $http) {
      $http.get("/polls/all")
     .then(function (response) {
-         
-        $scope.polls = response.data;
         
-       
-         
-         
-
-     //   console.log( $scope.polls[1].title + "  MY Obj: ");
-     //   console.log( $scope.polls[1].list[0].item + "  MY Obj item: ");
-     //   console.log( $scope.polls[1].list[0].count + "  MY Obj count: ");
-         for(var i=0;i<$scope.polls.length;i++){
-                 $scope.dataTable = new google.visualization.DataTable();
-                 $scope.dataTable.addColumn('string', 'Items');
-                 $scope.dataTable.addColumn('number', 'Votes');
-             for(var j=0;j<$scope.polls[i].list.length;j++) {
-                 $scope.dataTable.addRow([$scope.polls[i].list[j].item, $scope.polls[i].list[j].count]);
-             }
-             console.log($scope.dataTable  + " MY CHART OBJ.");
-             google.charts.setOnLoadCallback(drawVisualization($scope.dataTable,$scope.polls[1].title));
-         }
-           
     });
-    
-    
-     google.charts.load('current');   // Don't need to specify chart libraries!
-   
-        function drawVisualization(dataTable,Title) {
-            var wrapper = new google.visualization.ChartWrapper({
-              chartType: 'PieChart',
-              dataTable: dataTable,
-              options: {'title': Title},
-              containerId: 'pie'
-            });
-            wrapper.draw();
-          }
-          
-          
-          
-          
     
      
      $scope.getAll = function(){
@@ -52,6 +15,7 @@ myApp.controller('mainController', function($scope, $http) {
             $scope.polls = response.data;
         });
      } 
+     
      $scope.vote = function(poll, itemName, slectedItemObj) {
          //voting call here
          $scope.selectedName = slectedItemObj;
@@ -85,15 +49,59 @@ myApp.controller('mainController', function($scope, $http) {
     }; 
     
 
+     $scope.drawChart = function(poll, itemName) {
+         
+         google.charts.load('current');   // Don't need to specify chart libraries!
+
+         $scope.selectedName.item = itemName;
+         $scope.poll = poll;
+
+         $scope.string_API = "/polls/viewChart/" + itemName;
+         console.log("LOG Show drawChart: "+ $scope.string_API);
     
-   
-
-          
+         $http.get($scope.string_API)  //string 
+        .then(function (response) {
+            $scope.itemToDraw = response.data;
+            console.log("My Object to Draw "+$scope.itemToDraw  );
+        }); 
+    };
+    
+    
+    
+    
 }); 
+/*
+
+      function drawVisualization(dataTable,Title) {
+            var wrapper = new google.visualization.ChartWrapper({
+              chartType: 'PieChart',
+              dataTable: dataTable,
+              options: {'title': Title},
+              containerId: 'pie'
+            });
+            wrapper.draw();
+          }
+          
+          
+        function drawVisualization()
+        
+     //   console.log( $scope.polls[1].title + "  MY Obj: ");
+     //   console.log( $scope.polls[1].list[0].item + "  MY Obj item: ");
+     //   console.log( $scope.polls[1].list[0].count + "  MY Obj count: ");
+         for(var i=0;i<$scope.polls.length;i++){
+                 $scope.dataTable = new google.visualization.DataTable();
+                 $scope.dataTable.addColumn('string', 'Items');
+                 $scope.dataTable.addColumn('number', 'Votes');
+             for(var j=0;j<$scope.polls[i].list.length;j++) {
+                 $scope.dataTable.addRow([$scope.polls[i].list[j].item, $scope.polls[i].list[j].count]);
+             }
+             console.log($scope.dataTable  + " MY CHART OBJ.");
+             google.charts.setOnLoadCallback(drawVisualization($scope.dataTable,$scope.polls[1].title));
+         }
 
 
 
-
+*/
  
    
  
