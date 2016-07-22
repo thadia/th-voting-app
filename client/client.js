0e7e7fd57f4e937bd1064092a2a979f33c3e047a
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-22 18:52:28 +0000

    code in

--- client/client.js
+++ client/client.js
@@ -81,7 +81,7 @@ myApp.controller('mainController', function($scope, $http) {
     
     $scope.removePoll = function(pollName ) {
          $scope.pollName = pollName;
-         $scope.string_API = "/polls/remove/'"+$scope.pollName ;
+         $scope.string_API = "/polls/remove/"+$scope.pollName ;
          console.log("LOG New Poll: "+ $scope.string_API);
          $http.get($scope.string_API)  
         .then(function (response) {
