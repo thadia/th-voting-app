010bb36253a2dad1327be0d650b3f6860c1d992a
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-07-25 14:55:46 +0000

    code in

--- client/client.js
+++ client/client.js
@@ -53,7 +53,8 @@ myApp.controller('mainController', function($scope, $http) {
         .then(function (response) {
              $scope.userdata = null;
              $scope.getAll();
-             $window.location.href = '/';
+             $route.reload();
+
 
         }); 
     }; 
